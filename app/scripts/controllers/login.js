c89cff4186249cb4e7a5a59a081ea771af2de18c
Dmytro Matsiuk <dmat@i.ua>
2016-07-12 11:32:45 +0300

    add login page currently without styles

---
+++ app/scripts/controllers/login.js
@@ -0,0 +1,31 @@
+'use strict';
+
+angular.module('chatApp')
+    .controller('LoginCtrl', function ($scope, Auth, $location, mySocket) {
+        if (Auth.isLoggedIn()) {
+            $location.path('/');
+        }
+        $scope.user = {};
+        $scope.errors = {};
+
+        $scope.login = function (form) {
+            $scope.submitted = true;
+
+            if (form.$valid) {
+                Auth.login({
+                    email: $scope.user.email,
+                    password: $scope.user.password
+                })
+                    .then(function () {
+                        // Reconnect to socket as logged in user
+                        mySocket.reconnect();
+                        // Logged in, redirect to home
+                        $location.path('/');
+                    })
+                    .catch(function (err) {
+                        err = err.data;
+                        $scope.errors.other = err.message;
+                    });
+            }
+        };
+    });
